297bd00bf6ad63b740a0b9111910297307483ebd
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2022-11-21 22:57:29 +0400

    init

---
+++ pages/custom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>data</div>
+</template>
+
+<script setup lang="ts">
+import { onUnmounted, onMounted, ref } from 'vue'
+const visible = ref(true);
+onMounted(() => {
+  console.log('mounted');
+});
+onUnmounted(() => {
+  console.log('unmounted');
+});
+</script>
